6dd198a7ed75f5e6e2523743ce6d5a5af262b077
realityone <realityone@me.com>
2016-12-22 01:10:46 +0800

    use cffi to load functions from library

--- libdrafter/parser.py
+++ libdrafter/parser.py
@@ -1,13 +1,13 @@
 from __future__ import unicode_literals
 
-import ctypes
+import cffi
 
 
 class ParserError(Exception):
     pass
 
 
-class Parser(object):
+class Parser(cffi.FFI):
     @staticmethod
     def find_library():
         from ctypes.util import find_library
@@ -16,6 +16,34 @@ class Parser(object):
             raise ParserError('libdrafter(Drafter) not found')
         return libdrafter_path
 
-    def __init__(self, libdrafter_path=None):
+    def __init__(self, libdrafter_path=None, *args, **kwargs):
+        super(Parser, self).__init__(*args, **kwargs)
+
+        self.cdef(
+            """
+typedef struct drafter_result drafter_result;
+
+typedef enum {
+    DRAFTER_SERIALIZE_YAML = 0,
+    DRAFTER_SERIALIZE_JSON
+} drafter_format;
+
+typedef struct {
+    bool sourcemap;
+    drafter_format format;
+} drafter_options;
+
+int drafter_parse_blueprint_to(const char* source,
+                               char** out,
+                               const drafter_options options);
+int drafter_parse_blueprint(const char* source, drafter_result** out);
+char* drafter_serialize(drafter_result *res, const drafter_options options);
+void drafter_free_result(drafter_result* result);
+drafter_result* drafter_check_blueprint(const char* source);
+unsigned int drafter_version(void);
+const char* drafter_version_string(void);
+"""
+        )
+
         self.libdrafter_path = libdrafter_path or self.find_library()
-        self.libdrafter = ctypes.CDLL(libdrafter_path)
+        self.libdrafter = self.dlopen(self.libdrafter_path)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,5 +8,17 @@ class TestEncrypt(unittest.TestCase):
         self.parser = parser.Parser()
 
     def test_load(self):
+        methods = [
+            'drafter_parse_blueprint_to',
+            'drafter_parse_blueprint',
+            'drafter_serialize',
+            'drafter_free_result',
+            'drafter_check_blueprint',
+            'drafter_version',
+            'drafter_version_string'
+        ]
+
         self.assertTrue(self.parser.libdrafter_path)
         self.assertTrue(self.parser.libdrafter)
+        for m in methods:
+            self.assertIsNotNone(getattr(self.parser.libdrafter, m))
