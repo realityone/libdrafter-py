99e27204676a7c3acd8722a36d1abeac5bc9282d
realityone <realityone@me.com>
2016-12-24 16:52:19 +0800

    fit for python3

--- libdrafter/__init__.py
+++ libdrafter/__init__.py
@@ -1,4 +1,6 @@
-from parser import Parser
-from parser import ParserError
+from __future__ import absolute_import
+
+from .parser import Parser
+from .parser import ParserError
 
 __all__ = ['Parser', 'ParserError']
